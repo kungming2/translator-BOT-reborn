a1752fc36eaa7bc9d048ff1ccc1418f6da01938c
kungming2 <24950347+kungming2@users.noreply.github.com>
2025-10-08 14:51:37 -0700

    actually add content

--- commands/lookup_wp.py
+++ commands/lookup_wp.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-import time
-
+"""Simple command wrapper for Wikipedia lookup."""
 from config import logger
+from lookup.wp_utils import wikipedia_lookup
+from reddit_sender import comment_reply
 
 
-def handle(comment, instruo, komando, ajo):
-    print("Wikipedia lookup handler initiated.")
+def handle(comment, _instruo, komando, _ajo):
+    """Example:
+    Komando(name='wikipedia_lookup', data=['Sanxing (deities)'])]
+    """
+    logger.info("Wikipedia Lookup handler initiated.")
+    logger.info(f"[ZW] Bot: COMMAND: Wikipedia Lookup, from u/{comment.author}.")
+
+    wikipedia_data = wikipedia_lookup(komando.data)
+
+    if wikipedia_data:
+        comment_reply(comment, wikipedia_data)
+        logger.info(f"[ZW] Bot: COMMAND: Replied to comment `{comment.id}`.")
+
+    return
