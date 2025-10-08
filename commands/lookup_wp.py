ad4954b0f330ce83ce328d7aeabd29fa91ebd20b
kungming2 <24950347+kungming2@users.noreply.github.com>
2025-10-08 14:48:03 -0700

    Command wrapper for Wikipedia lookup

---
+++ commands/lookup_wp.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+import time
+
+from config import logger
+
+
+def handle(comment, instruo, komando, ajo):
+    print("Wikipedia lookup handler initiated.")
