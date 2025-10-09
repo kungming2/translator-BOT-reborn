de5bec335c83dcb1ec53da3857c9af1e3963c1e4
kungming2 <24950347+kungming2@users.noreply.github.com>
2025-10-09 16:28:44 -0700

    Properly built out command handler

--- commands/set.py
+++ commands/set.py
@@ -4,13 +4,15 @@
 !set is a mod-accessible means of setting the post flair. The mod's
 comment is removed by AutoModerator so it looks like nothing happened.
 """
+from . import update_language
 from config import logger
 from connection import is_mod
+from models.kunulo import Kunulo
 from reddit_sender import message_send
 
 
 def handle(comment, _instruo, komando, ajo):
-    print("Set handler initiated.")
+    logger.info("Set handler initiated.")
 
     # Check to see if the person calling this command is a moderator
     if not is_mod(comment.author):
@@ -19,17 +21,22 @@ def handle(comment, _instruo, komando, ajo):
 
     logger.info(f"[ZW] Bot: COMMAND: !set, from moderator u/{comment.author} on `{ajo.id}`.")
 
-    # Update the Ajo's language for a single-language post.
-    if ajo.type == 'single':
-        new_language = komando.data[0]  # Lingvo
-        ajo.set_language(new_language)
+    # Update the Ajo's language(s) post.
+    update_language(ajo, komando)
 
-        # Message the mod who called this command.
-        set_msg = f"The [post](https://redd.it/{ajo.id}) has been set to the language `{new_language.preferred_code}` (`{new_language.name}`)."
-        message_send(comment.author, subject='[Notification] !set command successful', body=set_msg)
-        logger.info("Informed moderator of command success.")
-    else:  # Defined multiple post.
-        # TODO logic here
-        pass
+    # Delete any pre-existing defined multiple or "Unknown" comment.
+    delete_tags = ['comment_defined_multiple', 'comment_unknown']
+    for tag in delete_tags:
+        kunulo_object = Kunulo.from_submission(ajo.submission)
+        kunulo_object.delete(tag)
+
+    # Message the mod who called this command.
+    new_language = komando.data[0]  # Lingvo
+    set_msg = (f"The [post](https://www.reddit.com{ajo.permalink}) has been set to the language "
+               f"`{new_language.preferred_code}` (`{new_language.name}`).")
+    message_send(comment.author, subject='[Notification] !set command successful', body=set_msg)
+    logger.info("Informed moderator of command success.")
 
     ajo.update_reddit()
+
+    return
