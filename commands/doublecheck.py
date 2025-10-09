0a39b592650d9dc0d95c4551103b23949ee68f40
kungming2 <24950347+kungming2@users.noreply.github.com>
2025-10-09 14:50:58 -0700

    Fully implemented command

--- commands/doublecheck.py
+++ commands/doublecheck.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-import time
+"""
+Allows posts to be set as "Needs Review".
+This is generally used when asking for reviews of one's work.
+"""
 
+from . import update_status
 from config import logger
+from models.kunulo import Kunulo
 
 
-def handle(comment, instruo, komando, ajo):
-    print("Doublecheck handler initiated.")
-    logger.info(f"[ZW] Bot: COMMAND: !doublecheck, from u/{comment.author}.")
-    current_time = time.time()
+def handle(comment, _instruo, komando, ajo):
+    logger.info("Doublecheck handler initiated.")
+    status_type = 'doublecheck'
+    logger.info(f"[ZW] Bot: COMMAND: !{status_type}, from u/{comment.author}.")
 
-    if ajo.type == 'single':
-        if ajo.status != "translated" and ajo.status != "doublecheck":
-            ajo.set_status('doublecheck')
-            ajo.set_time('doublecheck', current_time)
-    else:  # TODO Defined multiple logic here
-        pass
+    # Handler logic to update the post status.
+    update_status(ajo, komando, status_type)
 
-    # TODO Delete any claimed comment.
-
-    logger.info("[ZW] Bot: > Marked post as 'Needs Review.'")
+    # Update the Ajo and post.
+    logger.info(f"[ZW] Bot: > Marked post `{ajo.id}` as 'Needs Review.' "
+                f"(`{status_type}`)")
     ajo.update_reddit()
+
+    # Delete any previously claimed comment.
+    kunulo_object = Kunulo.from_submission(ajo.submission)
+    kunulo_object.delete('comment_claim')
+
+    return
