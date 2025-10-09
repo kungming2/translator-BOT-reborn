0f73adce1705425fa6fbde61ca83074b4495de96
kungming2 <24950347+kungming2@users.noreply.github.com>
2025-10-09 14:49:29 -0700

    Allow for specific languages to be passed to update_status (mostly for claim), and added an update_language for set and identify commands

--- commands/__init__.py
+++ commands/__init__.py
@@ -23,23 +23,34 @@ def discover_handlers():
             HANDLERS[cmd_name] = mod.handle
 
 
-def update_status(ajo, komando, status_type):
+def update_status(ajo, komando, status_type, specific_languages=None):
     """
     Shared function to handle status updates for different post types.
+    This function is used by: !translated, !doublecheck, !missing,
+                              and !claim
 
     Args:
         ajo: The Ajo object representing the post
         komando: Command object containing data
         status_type: Type of status to set
+        specific_languages: Optional list of language objects to update.
+                          If provided, these will be used instead of komando.data
     """
     current_time = time.time()
 
     if ajo.type == 'single':
+        # Skip if status is already set to the requested value
+        if hasattr(ajo, 'status') and ajo.status == status_type:
+            logger.debug(f"Status is already '{status_type}'. Skipping update.")
+            return
+
         ajo.set_status(status_type)
         ajo.set_time(status_type, current_time)
     else:
         if ajo.is_defined_multiple:
-            defined_languages = komando.data  # Lingvo object list
+            # Use specific_languages if provided, otherwise fall back to komando.data
+            defined_languages = specific_languages if specific_languages is not None else komando.data
+
             for language in defined_languages:
                 ajo.set_defined_multiple_status(language.preferred_code,
                                                 status_type)
@@ -49,6 +60,26 @@ def update_status(ajo, komando, status_type):
         else:
             logger.debug("Regular multiple post. Skipping...")
             pass
+
+
+def update_language(ajo, komando):
+    """
+    Shared function used to set a language. This will automatically
+    handle single and defined multiple posts. The data package will
+    generally be a list of Lingvos.
+
+    This is used by !set and !identify.
+    """
+    # Convert a list of single language items to a single object.
+    if len(komando.data) == 1:
+        languages_to_set = komando.data[0]
+    else:
+        languages_to_set = komando.data
+
+    # Update the Ajo's language.
+    ajo.set_language(languages_to_set)
+
+    return
 
 
 discover_handlers()
