daeb3b83d16f71a3bbc7ae1521017de341c7e852
kungming2 <24950347+kungming2@users.noreply.github.com>
2025-10-08 17:46:47 -0700

    Missing command handler completed

--- commands/missing.py
+++ commands/missing.py
@@ -4,27 +4,31 @@
 This handles the !missing command, which designates a post as missing
 content to be translated and messages the OP about it.
 """
-import time
-
+from . import update_status
 from connection import REDDIT
 from config import logger
 from reddit_sender import message_send
 from responses import RESPONSE
 
 
-def handle(comment, _instruo, _komando, ajo):
-    print("Missing handler initiated.")
-    logger.info(f"[ZW] Bot: COMMAND: !missing, from u/{comment.author} on `{ajo.id}`.")
-    current_time = time.time()
+def handle(comment, _instruo, komando, ajo):
+    status_type = 'missing'
+    logger.info("Missing handler initiated.")
+    logger.info(f"[ZW] Bot: COMMAND: !{status_type}, from u/{comment.author} on `{ajo.id}`.")
     original_poster = REDDIT.redditor(ajo.author)
 
-    total_message = RESPONSE['MSG_MISSING_ASSETS'].format(oauthor=original_poster, opermalink=ajo.permalink)
+    # Handler logic to update the post status.
+    update_status(ajo, komando, status_type)
+
+    # Format and send the message to the OP letting them know.
+    total_message = RESPONSE['MSG_MISSING_ASSETS'].format(oauthor=original_poster,
+                                                          opermalink=ajo.permalink)
     message_send(original_poster,
                  subject='A message from r/translator regarding your translation request',
                  body=total_message)
 
-    ajo.set_status("missing")
-    ajo.set_time('missing', current_time)
-
-    logger.info(f"[ZW] Bot: > Marked post `{ajo.id}` by u/{original_poster} as missing assets and messaged them.")
+    logger.info(f"[ZW] Bot: > Marked post `{ajo.id}` by u/{original_poster} "
+                f"as missing assets and messaged them.")
     ajo.update_reddit()
+
+    return
