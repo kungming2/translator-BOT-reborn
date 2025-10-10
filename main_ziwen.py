50cbb469a2ce3bade779c4f24e56bff1564ca8ff
kungming2 <24950347+kungming2@users.noreply.github.com>
2025-10-10 12:21:03 -0700

    First complete version of main Ziwen routine

--- main_ziwen.py
+++ main_ziwen.py
@@ -13,9 +13,13 @@ import traceback
 from config import logger
 from connection import REDDIT, credentials_source
 from database import record_activity_csv
+from edit_tracker import edit_tracker, progress_tracker
 from error import error_log_extended
-from processes.ziwen_bot import ziwen_bot
+from processes.ziwen_commands import ziwen_commands
+from processes.ziwen_messages import ziwen_messages
+from processes.ziwen_posts import ziwen_posts
 from time_handling import time_convert_to_string
+from verification import verification_parser
 
 
 if __name__ == '__main__':
@@ -23,17 +27,28 @@ if __name__ == '__main__':
 
     try:
         try:
-            # Process comments.
-            ziwen_bot()
+            # First it processes the titles of new posts.
+            ziwen_posts()
 
-            # Then it checks its messages (generally for new subscription lookups).
-            # ziwen_messages()
+            # Then it checks for any edits to comments.
+            edit_tracker()
+            progress_tracker()
+
+            # Process comments and acts on commands.
+            ziwen_commands()
+
+            # Then it checks its messages (generally for new
+            # subscriptions for language notifications).
+            ziwen_messages()
+
+            # Process verification requests.
+            verification_parser()
 
             # Record API usage limit.
             probe = REDDIT.redditor(credentials_source['USERNAME']).created_utc
             used_calls = REDDIT.auth.limits["used"]
 
-            # Record memory usage at the end of an isochronism.
+            # Record memory usage at the end of a run.
             mem_num = psutil.Process(os.getpid()).memory_info().rss
             mem_usage = "{:.2f} MB".format(mem_num / (1024 * 1024))
             logger.info(
@@ -45,7 +60,7 @@ if __name__ == '__main__':
 
             # Format the error text.
             error_entry = traceback.format_exc()
-            error_log_extended(error_entry, 'Ziwen')
+            error_log_extended(error_entry, 'Ziwen Main')
         else:
             # Package data for this run and write it to a record.
             elapsed_time = round((time.time() - start_time) / 60, 2)
