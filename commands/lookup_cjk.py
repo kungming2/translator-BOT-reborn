d3ad0bc272a49175e8dcf217e801af646e3ebd6a
kungming2 <24950347+kungming2@users.noreply.github.com>
2025-10-10 12:16:37 -0700

    Made lookup helper functions to be async

--- commands/lookup_cjk.py
+++ commands/lookup_cjk.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 """Command wrapper for CJK languages lookup."""
+import asyncio
 import random
-import time
 
 from config import logger, load_settings, Paths
+from languages import converter
 from lookup.ja import ja_character, ja_word
 from lookup.ko import ko_word
 from lookup.zh import zh_character, zh_word
@@ -29,28 +30,28 @@ def find_cjk_language(preferred_code):
     return None
 
 
-def _lookup_chinese_term(term):
+async def _lookup_chinese_term(term):
     """Perform Chinese character or word lookup."""
     if len(term) == 1:
-        return zh_character(term)
-    return zh_word(term)
+        return await zh_character(term)
+    return await zh_word(term)
 
 
-def _lookup_japanese_term(term):
+async def _lookup_japanese_term(term):
     """Perform Japanese character or word lookup."""
     if len(term) == 1:
         return ja_character(term)
-    return ja_word(term)
+    return await ja_word(term)
 
 
-def _lookup_korean_term(term):
+async def _lookup_korean_term(term):
     """Perform Korean word lookup."""
     return ko_word(term)
 
 
-def _rate_limit_delay():
+async def _rate_limit_delay():
     """Add randomized delay between lookup requests."""
-    time.sleep(random.randint(3, 10))
+    await asyncio.sleep(random.randint(3, 10))
 
 
 def _get_lookup_language(instruo, ajo):
@@ -69,8 +70,9 @@ def _get_lookup_language(instruo, ajo):
     return ajo.lingvo
 
 
-def _perform_lookups(cjk_language, search_terms):
-    """Perform lookups based on CJK language type."""
+async def perform_cjk_lookups(cjk_language, search_terms):
+    """Perform lookups based on CJK language type.
+    search_terms must be a list of strings."""
     lookup_functions = {
         'Chinese': _lookup_chinese_term,
         'Japanese': _lookup_japanese_term,
@@ -82,24 +84,30 @@ def _perform_lookups(cjk_language, search_terms):
         return []
 
     results = []
+    logger.info(f"Passing {search_terms} to the {cjk_language} "
+                f"lookup function...")
     for term in search_terms:
-        result = lookup_func(term)
+        result = await lookup_func(term)
         if result:
             results.append(result)
-        _rate_limit_delay()
+        await _rate_limit_delay()
 
     return results
 
 
-def _format_reply(ajo, lookup_results):
+def _format_reply(lookup_results, ajo=None):
     """Format the reply body with lookup results."""
     anchor_tag = RESPONSE.ANCHOR_CJK
     formatted_results = '\n\n'.join(lookup_results)
-    author_tag = (
-        f"*u/{ajo.author} (OP), the following lookup results "
-        "may be of interest to your request.*\n\n"
-    )
-    return author_tag + formatted_results + RESPONSE.BOT_DISCLAIMER + anchor_tag
+
+    if not ajo:
+        return formatted_results
+    else:
+        author_tag = (
+            f"*u/{ajo.author} (OP), the following lookup results "
+            "may be of interest to your request.*\n\n"
+        )
+        return author_tag + formatted_results + RESPONSE.BOT_DISCLAIMER + anchor_tag
 
 
 def handle(comment, instruo, komando, ajo):
@@ -122,10 +130,19 @@ def handle(comment, instruo, komando, ajo):
         return
 
     # Perform lookups for all search terms
-    lookup_results = _perform_lookups(cjk_language, komando.data)
+    lookup_results = asyncio.run(perform_cjk_lookups(cjk_language, komando.data))
 
     # Reply if we have information to provide
     if lookup_results:
-        reply_body = _format_reply(ajo, lookup_results)
+        reply_body = _format_reply(lookup_results, ajo)
         comment_reply(comment, reply_body)
         logger.info(f"[ZW] Bot: >> Looked up the term(s) in {cjk_language}.")
+
+
+if __name__ == '__main__':
+    while True:
+        my_language = input("Enter a CJK language code or name to use: ")
+        my_input = input("Please enter a string to lookup: ")
+        search_language = converter(my_language).name
+        test_search_data = asyncio.run(perform_cjk_lookups(search_language, [my_input]))
+        print(_format_reply(test_search_data))
