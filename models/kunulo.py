8e1b26cfacc1adcd891f0a916f736a3b76da99ba
kungming2 <24950347+kungming2@users.noreply.github.com>
2025-10-18 21:16:41 -0700

    More notes

--- models/kunulo.py
+++ models/kunulo.py
@@ -339,7 +339,7 @@ def get_submission_from_comment(comment_reference):
 
 if __name__ == "__main__":
     while True:
-        test_url = input("Enter the URL of the Reddit post to test: ")
+        test_url = input("Enter the URL of the Reddit post with comments to test: ")
         submission_id = test_url.split("comments/")[1].split("/")[0]
         test_post = REDDIT_HELPER.submission(id=submission_id)
         test_kunulo = Kunulo.from_submission(test_post)
